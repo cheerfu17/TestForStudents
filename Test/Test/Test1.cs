c262b591705c7d8f6f6d80763ac64ca9081c2534
cheerful17 <demonhpru@gmail.com>
2020-11-27 16:10:48 +0500

    Add files via upload

---
+++ Test/Test/Test1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test
+{
+
+    public partial class Test1 : Form
+    {
+
+
+        Form1 f = new Form1();
+
+        //путь будет слетать, нужно будет что-то делать с этим
+        string filePathCollege = @"D:\bin\student\CheerfulFolder\TestForStudents-main\Test\Test\score.txt";
+        string filePathHome = @"D:\Project\C#\TestForStudents-main\Test\Test\score.txt";
+        string filePath = "";
+
+        int flag = 1;
+
+        
+        int eq = 0; // сюда записываются правильные ответы
+        int score = 0;
+        int rCheck = 0;
+
+        public Test1()
+        {
+
+            InitializeComponent();
+            filePath = filePathHome;
+            //как сделать иначе хз, но это по любому костыль
+            Theme1 t = new Theme1(label1, radioButton1, radioButton2, radioButton3, radioButton4, flag, button1);
+            eq = t.rightAnswer;
+
+            f.Close();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //сравнивание правильного ответа и выбранной радиокнопкой
+            if (eq == rCheck)
+            {
+                score++;
+            }
+            //ничего лучше и проще для проверки на конец теста придумать не мог, пока так
+            if (button1.Text == "Закончить тест")
+            {   
+                //показывает сколько правильных ответов дал пользователь
+                MessageBox.Show($"Вы ответили правильно на {score} вопросов");
+
+
+                //запись результатов в txt
+                File.AppendAllText(filePath, $"В тесте 1 вы набрали {score} баллов\n");
+
+                f.DataUpdate();
+                //закрытие формы
+                this.Close();
+            }
+
+
+            flag += 1;
+
+            // В конструктор передаются необходимые ссылки на объекты с флагом, дальше конструктор обрабатывает флаг и направляет на нужный вопрос
+            Theme1 t = new Theme1(label1, radioButton1, radioButton2, radioButton3, radioButton4, flag, button1);
+            eq = t.rightAnswer;
+        }
+
+
+        //блок для определения выбранной радио-кнопки
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)  
+        {
+            rCheck = 1;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            rCheck = 2;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            rCheck = 3;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            rCheck = 4;
+        }
+    }
+}
---
+++ Test/Test/Theme2.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Test
+{
+    class Theme2 : Form1
+    {
+
+        Label lb;
+        RadioButton b1;
+        RadioButton b2;
+        RadioButton b3;
+        RadioButton b4;
+        Button b;
+
+        int flag = 0;
+        public int rightAnswer;
+
+
+        //конструктор класса для принятия ссылок на объекты, так с ними можно будет работать
+        public Theme2(Label _lb, RadioButton _b1, RadioButton _b2, RadioButton _b3, RadioButton _b4, int _flag, Button _b)
+        {
+
+            lb = _lb;
+            b1 = _b1;
+            b2 = _b2;
+            b3 = _b3;
+            b4 = _b4;
+            flag = _flag;
+            b = _b;
+
+
+            //загрузка нужного вопроса
+            switch (flag)
+            {
+                case 1:
+                    Qes1();
+                    break;
+                case 2:
+                    Qes2();
+                    break;
+                case 3:
+                    Qes3();
+                    break;
+                case 4:
+                    Qes4();
+                    break;
+                case 5:
+                    Qes5();
+                    break;
+                case 6:
+                    Qes6();
+                    break;
+                case 7:
+                    Qes7();
+                    break;
+                case 8:
+                    Qes8();
+                    break;
+                case 9:
+                    Qes9();
+                    break;
+                case 10:
+                    Qes10();
+                    break;
+            }
+
+
+        }
+
+        /*Сами вопросы. Функции изменяют тест на форме и записывают в данные класса правильный ответ, потом этот ответ можно будет достать из других классов 
+        P.S знаю что названия методов нужно писать с маленькой буквы, лень изменять :/ */
+        public void Qes1()
+        {
+            lb.Text = "Что такое цикл?";
+            b1.Text = "Конструкция котороя позволяет многократно выполнять набор определенных команд";
+            b2.Text = "Конструкция котороя производит операции над числами";
+            b3.Text = "Оператор условной конструкции";
+            b4.Text = "Последовательность команд";
+            rightAnswer = 1;
+        }
+        public void Qes2()
+        {
+            lb.Text = "Выберите верную конструкцию цикла с предусловием";
+            b1.Text = "do{}while(условие);";
+            b2.Text = "while(условие) {};";
+            b3.Text = "for(;;){}";
+            b4.Text = "Нет верного ответа";
+            rightAnswer = 2;
+        }
+        public void Qes3()
+        {
+            lb.Text = "Что представляет из себя цикл for";
+            b1.Text = "Цикл с предусловием";
+            b2.Text = "Цикл с постусловием";
+            b3.Text = "Цикл с счетчиком и условием";
+            b4.Text = "Цикл для перебора значений в списках\\массивах";
+            rightAnswer = 3;
+        }
+        public void Qes4()
+        {
+            lb.Text = "Выберите рузультат выполнения следующй программы int a = 0; for(int i = 0; i < 10; i++) {a = i+2} Console.WriteLine(\"a\");";
+            b1.Text = "11";
+            b2.Text = "10";
+            b3.Text = "9";
+            b4.Text = "12";
+            rightAnswer = 1;
+        }
+        public void Qes5()
+        {
+            lb.Text = "Выберите верную конструкцию цикла с постусловием";
+            b1.Text = "do{}while(условие);";
+            b2.Text = "while(условие) {};";
+            b3.Text = "for(;;){}";
+            b4.Text = "Нет верного ответа";
+            rightAnswer = 1;
+
+        }
+        public void Qes6()
+        {
+            lb.Text = "Выберите верный способ задать бесконечный цикл";
+            b1.Text = "while(true) {}";
+            b2.Text = "do {} while(true);";
+            b3.Text = "for(;;) {}";
+            b4.Text = "Все варианты верны";
+            rightAnswer = 4;
+        }
+        public void Qes7()
+        {
+            lb.Text = "Для того чтобы принудительно выйти из цикла используется оператор";
+            b1.Text = "continue";
+            b2.Text = "break";
+            b3.Text = "stop";
+            b4.Text = "exit";
+            rightAnswer = 2;
+        }
+        public void Qes8()
+        {
+            lb.Text = "Для того чтобы принудительно перейти к следующей итерации цикла используется оператор";
+            b1.Text = "continue";
+            b2.Text = "break";
+            b3.Text = "stop";
+            b4.Text = "exit";
+            rightAnswer = 1;
+        }
+        public void Qes9()
+        {
+            lb.Text = "Цикл для перебора значений во множествах называется";
+            b1.Text = "for";
+            b2.Text = "while";
+            b3.Text = "for each";
+            b4.Text = "do-while";
+            rightAnswer = 3;
+        }
+        public void Qes10()
+        {
+            lb.Text = "Выберите верную конструкцию совместного цикла";
+            b1.Text = "do{}while(условие);";
+            b2.Text = "while(условие) {};";
+            b3.Text = "for(;;){}";
+            b4.Text = "foreach ([тип] [название переменной] in [множество] {} )";
+            rightAnswer = 4;
+            b.Text = "Закончить тест";
+        }
+
+
+
+    }
+}
